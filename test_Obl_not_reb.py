de491376cc00a447ad17eb2dc9e52d7ba62f77db
RomanSPbGASU <romanevsin@yandex.ru>
2020-04-30 20:04:41 +0300

    Fix bug with pow second argument

--- test_Obl_not_reb.py
+++ test_Obl_not_reb.py
@@ -10,10 +10,10 @@ class Test(TestCase):
         test_expr2 = recursive_cleaning_internal(Add(3, Mul(sin(x), 5)), x)
         test_expr3 = recursive_cleaning_internal(Add(3, Mul(sin(x), x)), x)
         test_expr4 = recursive_cleaning_internal(cos(Add(3, Mul(sin(x), x))), x)
-        test_expr5 = recursive_cleaning_internal(Pow(cos(Add(3, Mul(sin(x), x)))), x)
+        test_expr5 = recursive_cleaning_internal(Pow(cos(Add(3, Mul(sin(x), x))), 3), x)
 
         self.assertEqual(test_expr, sin(x))
         self.assertEqual(test_expr2, sin(x))
         self.assertEqual(test_expr3, Mul(sin(x), x))
         self.assertEqual(test_expr4, cos(Mul(sin(x), x)))
-        self.assertEqual(test_expr5, Pow(cos(Mul(sin(x), x))))
+        self.assertEqual(test_expr5, Pow(cos(Mul(sin(x), x)), 3))
